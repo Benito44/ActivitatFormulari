5f6ec66e735958f8bc172f6e62e9d5df2355b99c
BenitoMF <b.martinez2@sapalomera.cat>
2024-02-01 16:51:56 +0100

    cosas

--- Cantina/Cantina/Cantina.js
+++ Cantina/Cantina/Cantina.js
@@ -1,7 +1,52 @@
 "use strict";
 
 function init() {}
-
+document.getElementById('importJSON').addEventListener('click', function() {
+	const fileInput = document.getElementById('fileInput');
+	
+	// Verificar si se ha seleccionado un archivo
+	if (fileInput.files.length === 0) {
+	  alert('Por favor selecciona un archivo JSON.');
+	  return;
+	}
+	
+	const file = fileInput.files[0];
+	const reader = new FileReader();
+  
+	reader.onload = function(event) {
+	  const jsonData = JSON.parse(event.target.result);
+	  renderTable(jsonData);
+	};
+  
+	reader.readAsText(file);
+  });
+  
+  function renderTable(data) {
+	const tbody = document.querySelector('#dataTable tbody');
+	tbody.innerHTML = '';
+  
+	data.forEach(item => {
+	  const row = document.createElement('tr');
+	  row.innerHTML = `
+		<td>${item.Num}</td>
+		<td>${item.Data}</td>
+		<td>${item.NIF}</td>
+		<td>${item.Client}</td>
+		<td>${item.Telefon}</td>
+		<td>${item['E-mail']}</td>
+		<td>${item.Subtotal}</td>
+		<td>${item.Dte}</td>
+		<td>${item['Base I']}</td>
+		<td>${item.IVA}</td>
+		<td>${item.Total}</td>
+		<td>${item.P}</td>
+		<td>${item.Acc}</td>
+	  `;
+	  tbody.appendChild(row);
+	});
+  }
+  
+  
 document.getElementById("tancar").addEventListener("click", reservaF);
 document.getElementById("tancar2").addEventListener("click", platsDiaF);
 document.getElementById("eliminar").addEventListener("click", reservaT);
